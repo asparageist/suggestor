bb21918d1d6e226dfcafd65b512750861facbc17
Joseph Murray <josephwmurray@gmail.com>
2023-01-28 18:33:29 -0800

    create js collecting data from user

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
 window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
-    const side1 = parseInt(document.querySelector("input#side1").value);
-    const side2 = parseInt(document.querySelector("input#side2").value);
-    const side3 = parseInt(document.querySelector("input#side3").value);
-    let triangle;
-    if (side1 && side2 && side3) {
-      if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1) {
-        if (side1 === side2 && side2 === side3) {
-          triangle = "equlilateral";
-        } else if (side1 === side2 || side2 === side3 || side1 === side3) {
-          triangle = "isosceles";
-        } else {
-          triangle = "scalene";
-        }
-      } else {
-          triangle = "ain't no triangle";
-      }
-  } else {
-      triangle = ("fill the fields");
-  }
-  document.querySelector("span#isTriangle").innerText = triangle;
+    const hand = document.getElementById("paws").value;
+    const mental = document.getElementById("brainType").value;
+    const colors = document.getElementById("sleep").value;
+    const companion = document.getElementById("pets").value;
+    const vice = document.getElementById("recreation").value;
+    console.log(hand);
+    console.log(mental);
+    console.log(colors);
+    console.log(companion);
+    console.log(vice);
+    //const side2 = parseInt(document.querySelector("input#side2").value);
+    //const side3 = parseInt(document.querySelector("input#side3").value);
+    let language;
+    if (mental === "Creative") {
+      language = "information about option 1";
+    } else if (mental === "Logic") {
+      language = "information about option 2";
+    } else {
+      language = "information about option 3";
+    }
+  document.querySelector("span#languageChoice").innerText = language;
 };
 }
