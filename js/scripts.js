ce5ca794b190d4dcf4bc41f106373deb503d8220
Joseph Murray <josephwmurray@gmail.com>
2023-01-28 21:26:57 -0800

    create a readme file

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,31 @@ window.onload = function() {
     const colors = parseInt(document.getElementById("sleep").value);
     const companion = document.getElementById("pets").value;
     const vice = document.getElementById("recreation").value;
-    
-    function alignIt() {
-      if (hand === "Right") {
-        let txt = document.getElementById("language");
-        txt.style.textAlign="right";
-      } else if (hand === "Left") {
-        let txt = document.getElementById("language");
-        txt.style.textAlign="left";
-      } else {
-        txt.style.textAlign="center";
-      }
-    }
+
+    let language1;
+    let language2;
+    let language3;
+    let noise;
+    let party;
+
+
     //===========================================================================
     
-    let language;
+   
     if (mental === "Creative") {
-      language = "Might as well start with Python! The level of simplicity in its code structure allows for a coding experience that your personality type would find attractive! Have a blast making your code your own! The only thing Python is missing right now is your masterful manipulation! Make a million programs in one day that all scream Designer Dream! All you need is to VICE for inspiration and answer the NOISE in your soul, and you can own the world!";
+      language1 = "Might as well start with Python! The level of simplicity in its code structure allows for a coding experience that your personality type would find attractive! Have a blast making your code your own! The only thing Python is missing right now is your masterful manipulation! Make a million programs in one day that all scream Designer Dream! All you need is to ";
+      language2 = " for inspiration and answer the ";
+      language3 = " in your soul, and you can own the world!";
     } else if (mental === "Logic") {
-      language = "Check out Ruby! Its focus on productivity aligns perfectly with your personality type. Get in there and get it done! Once you master Ruby, you can do anything! It's a gateway to virtually infinite possibilities. So, VICE and ignore the NOISE emanating from the other room and hunker down to a productivity party with Ruby, The Object-oriented Scripting Language!";
+      language1 = "Check out Ruby! Its focus on productivity aligns perfectly with your personality type. Get in there and get it done! Once you master Ruby, you can do anything! It's a gateway to virtually infinite possibilities. So, ";
+      language2 = " and ignore the ";
+      language3 = " emanating from the other room and hunker down to a productivity party with Ruby, The Object-oriented Scripting Language!";
     } else {
-      language = "Sounds like you'd love to get your hands on some JavaScript! Its impossibly complicated user interface is just the thing you need to pull all your hair out trying to figure out how to align all the text to the right when someone says they're right handed! Is the background blue? Probably not, but it will be some day! Just kick back in anxious agony, dreaming of some day when you can VICE, and try to drown out the NOISE from your starving dependants whose very existence you've all but forgotten about and read the same forums over and over again wishing you had started earlier because the deadline is looming with JavaScript! Huzzah!";
+      language1 = "Sounds like you'd love to get your hands on some JavaScript! Its impossibly complicated user interface is just the thing you need to pull all your hair out trying to figure out how to align all the text to the right when someone says they're right handed! Is the background blue? Probably not, but it will be some day! Just kick back in anxious agony, dreaming of some day when you can ";
+      language2 = " and try to drown out the ";
+      language3 = " from your starving dependants whose very existence you've all but forgotten about and read the same forums over and over again wishing you had started earlier because the deadline is looming with JavaScript! Huzzah!";
     }
-    let noise;
+
     if (companion === "Cats") {
       noise = "meowing";
     } else if (companion === "Dogs") {
@@ -41,7 +43,7 @@ window.onload = function() {
     } else {
       noise = "thick and viscous silence, pregnant with the regret you feel for having chosen solitude over any form of animal companionship";
     }
-    let party;
+
     if (vice === "Grape") {
       party = "pop a bottle of bubbly";
     } else if (vice === "Grain") {
@@ -51,6 +53,7 @@ window.onload = function() {
     } else {
       party = "drink some tea or something, I guess. I don't know...";
     }
-  document.querySelector("span#languageChoice").innerText = language;
+  const wholeOutput = language1 + party + language2 + noise + language3;
+  document.querySelector("span#languageChoice").innerText = wholeOutput;
 };
 }
---
+++ readme.md
@@ -0,0 +1,34 @@
+# _Portfolio_
+
+# https://github.com/asparageist/suggestor
+
+#### By Joseph Murray
+
+#### A page that utilizes JavaScript, Web API and event handlers to suggest a starting language for a new coder to attempt to pick up.
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+* Markdown
+* Bootstrap
+
+## Description
+
+This page is intended to showcase my understanding of JavaScript to this point. This page asks visitors questions about themselves in order to determine which programming language they'd likely work with best. It uses data collected from select boxes all triggered by a single button to display a customizable paragraph in response.
+
+## Setup/Installation Requirements
+
+* Download my repository to your Desktop
+* Open suggestor.html
+
+## Known Bugs
+
+* Difficulty getting CSS formatting to change on demand
+
+## License
+
+* Any questions, comments, concerns or contributions, contact the author at josephwmurray@gmail.com
+
+Copyright (c) 1/28/2023 Joseph Murray
