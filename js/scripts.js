ba30a51c7cf8b1946ca06763731ebdb5acdb252c
Joseph Murray <josephwmurray@gmail.com>
2023-01-28 19:55:29 -0800

    begin to apply css options to output text

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
 window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
+
     const hand = document.getElementById("paws").value;
     const mental = document.getElementById("brainType").value;
-    const colors = document.getElementById("sleep").value;
+    const colors = parseInt(document.getElementById("sleep").value);
     const companion = document.getElementById("pets").value;
     const vice = document.getElementById("recreation").value;
-    console.log(hand);
-    console.log(mental);
-    console.log(colors);
-    console.log(companion);
-    console.log(vice);
-    //const side2 = parseInt(document.querySelector("input#side2").value);
-    //const side3 = parseInt(document.querySelector("input#side3").value);
+    
+    function alignIt() {
+      if (hand === "Right") {
+        let txt = document.getElementById("language");
+        txt.style.textAlign="right";
+      } else if (hand === "Left") {
+        let txt = document.getElementById("language");
+        txt.style.textAlign="left";
+      } else {
+        txt.style.textAlign="center";
+      }
+    }
+    //===========================================================================
+    
     let language;
     if (mental === "Creative") {
       language = "information about option 1";
@@ -20,6 +28,28 @@ window.onload = function() {
       language = "information about option 2";
     } else {
       language = "information about option 3";
+    }
+    let noise;
+    if (companion === "Cats") {
+      noise = "meowing";
+    } else if (companion === "Dogs") {
+      noise = "barking";
+    } else if (companion === "Birds") {
+      noise = "chirping";
+    } else if (companion === "Other") {
+      noise = "repellant gurgling";
+    } else {
+      noise = "thick and viscous silence, pregnant with the regret you feel for having chosen solitude over any form of animal companionship";
+    }
+    let party;
+    if (vice === "Grape") {
+      party = "pop a bottle of bubbly";
+    } else if (vice === "Grain") {
+      party = "crack open a cold one";
+    } else if (vice === "Green") {
+      party = "spark some dank nugs";
+    } else {
+      party = "drink some tea or something, I guess. I don't know";
     }
   document.querySelector("span#languageChoice").innerText = language;
 };
